c398da4432ecc4286de9ed5e6364055beb6b2fe8
John Shearing <johnshearing@gmail.com>
2025-04-17 00:21:27 -0400

    update

--- VideoRAG/_2_query.py
+++ VideoRAG/_2_query.py
@@ -7,7 +7,6 @@ warnings.filterwarnings("ignore")
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
 # Not needed here if this is already set in .bashrc
-# Please enter your openai key
 # os.environ["OPENAI_API_KEY"] = ""
 
 from videorag._llm import *
@@ -17,7 +16,7 @@ from videorag import VideoRAG, QueryParam
 if __name__ == '__main__':
     multiprocessing.set_start_method('spawn')
 
-    query = "Does a person's feelings effect what happens in that person's life?"
+    query = "What is law of attraction?"
     param = QueryParam(mode="videorag")
     # if param.wo_reference = False, VideoRAG will add reference to video clips in the response
     param.wo_reference = True
